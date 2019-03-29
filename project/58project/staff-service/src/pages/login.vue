f0b8e6aa95262722f2e6bbeb0f8b74e3ff7bacdf
tangchao <magicheart@163.com>
2019-03-29 20:23:43 +0800

    修改登录长度限制

--- project/58project/staff-service/src/pages/login.vue
+++ project/58project/staff-service/src/pages/login.vue
@@ -3,10 +3,10 @@
         <div class="login-box">
             <el-form ref="form" :rules="rules" :model="form" label-width="80px">
                 <el-form-item label="用户名" prop="username">
-                    <el-input v-model="form.username" size="medium" type="text" placeholder="请输入用户名"></el-input>
+                    <el-input v-model="form.username" size="medium" :maxlength="20" type="text" placeholder="请输入账号"></el-input>
                 </el-form-item>
                 <el-form-item label="密码" prop="password">
-                    <el-input v-model="form.password" size="medium" type="password" placeholder="请输入密码"></el-input>
+                    <el-input v-model="form.password" size="medium" :maxlength="100" type="password" placeholder="请输入密码"></el-input>
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary" size="medium" @click="submitForm('form')">登录</el-button>
@@ -44,15 +44,15 @@ export default {
                 if (valid) {
                     this.isLoaded = true
                     try{
-                        
+
                         await loginService.getToken(this.form.username, this.form.password)
-                        
-                        
+
+
                         this.$message({
                             type: 'success',
                             message: '登陆成功！'
                         });
-                        
+
                         _this.$router.push('/');
                     }catch(e){
                         this.$message({
@@ -90,7 +90,7 @@ export default {
             box-shadow: 0 2px 3px 0 rgba(0,0,0,.2);
         }
     }
-    
+
 </style>
 
 
