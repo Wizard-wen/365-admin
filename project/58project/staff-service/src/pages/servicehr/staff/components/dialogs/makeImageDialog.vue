ca1aec29c582342e4d4eb6c2f9b3924c1c4420ae
DESKTOP-8CJ0PUQ\songxiwen_L_Win <songxiwen@ihmair.cn>
2019-04-15 17:08:13 +0800

    提交

--- project/58project/staff-service/src/pages/servicehr/staff/components/dialogs/makeImageDialog.vue
+++ project/58project/staff-service/src/pages/servicehr/staff/components/dialogs/makeImageDialog.vue
@@ -1,33 +1,29 @@
 <template>
-    <!-- <el-dialog 
-        :title="makeImageDialogTitle" 
-        :visible.sync="openMakeImageDialog"
-        :show-close="false"
-        :close-on-press-escape="false"
-        :close-on-click-modal="false">
-        <div class="makedImage" ref="makedImage">
-            <div>{{openMakeImage.name}}</div>
+    <div v-if="openMakeImageDialog" class="imageBox" v-loading="isMakingImage">
+        <div class="imageBox-contains">
+            <div ref="imageCutBox" class="imageCutBox canvasDiv" :style="{zIndex: isImage? '-200': '200'}">
+                <div>{{openMakeImage.name}}</div>
+            </div>
+
+            <div class="imageCutBox" :style="{zIndex: isImage? '200' : '-200'}">
+                <img :src="canvasImage" alt="" class="canvas-img">
+            </div>
+
+            <div class="imageBox-footer">
+                <el-button @click="closeMakeImageDialog">取 消</el-button>
+                <a :href="canvasImage" download="logo.png">下载图片</a>
+            </div>
         </div>
-        <div slot="footer" class="dialog-footer">
-            <el-button @click="closeMakeImageDialog">取 消</el-button>
-            <el-button type="primary" >下载</el-button>
-        </div>
-    </el-dialog> -->
-    <div v-if="openMakeImageDialog" class="makedImage" ref="makedImage">
-        <div  >
-            <div>{{openMakeImage.name}}</div>
-        </div>
-        <div  class="dialog-footer">
-            <el-button @click="closeMakeImageDialog">取 消</el-button>
-            <a :href="img" download="logo.png">下载图片</a>
-        </div>
-        <img :src="img" alt="" class="img">
+
+
     </div>
 </template>
 <script>
+
 import {hrService} from '../../../../../../common'
 import html2canvas from 'html2canvas';
 import axios from 'axios'
+
 export default {
     props: {
         //是否打弹出框
@@ -54,8 +50,10 @@ export default {
     },
     data(){
         return {
-            canvasImg:'',
-            img: ''
+            canvasBase64Img:'',//html2canvas生成的图片
+            canvasImage: '',//已上传到后台的图片
+            isImage:false,//是否已经生成图片
+            isMakingImage: false,
         }
     },
     methods: {
@@ -63,62 +61,83 @@ export default {
         closeMakeImageDialog(){
             this.$emit('closeMakeImageDialog')
         },
-        async changeImage(){
-            let _this = this
-            await html2canvas(_this.$refs.makedImage).then(function(canvas){
-                console.log(canvas.toDataURL("image/png"))
-                _this.canvasImg = canvas.toDataURL("image/png");
-            });
-        }
     },
-     mounted(){
-        //  debugger
+    async mounted(){
         let _this = this
-             html2canvas(_this.$refs.makedImage).then(function(canvas){
-                console.log(canvas.toDataURL("image/png"))
-                _this.canvasImg = canvas.toDataURL("image/png");
-                var bytes = window.atob(_this.canvasImg.split(',')[1]);
-                var array = [];
-                for(var i = 0; i < bytes.length; i++){
-                    array.push(bytes.charCodeAt(i));
-                }
-                var blob = new Blob([new Uint8Array(array)], {type: 'image/png'});
-                // var fd = new FormData();
-                // fd.append('file',blob, Date.now() + '.jpg');
-                let data = new FormData()
-                data.append("file", blob)
+        this.isMakingImage = true
+        await html2canvas(_this.$refs.imageCutBox).then(async function(canvas){
+            //生成base64图片
+            _this.canvasBase64Img = canvas.toDataURL("image/png");
+            
+            //base64 -> blob
+            var bytes = window.atob(_this.canvasBase64Img.split(',')[1]);
+            var array = [];
+            for(var i = 0; i < bytes.length; i++){
+                array.push(bytes.charCodeAt(i));
+            }
+            var blob = new Blob([new Uint8Array(array)], {type: 'image/png'});
 
-                // data.file = _this.canvasImg
-                console.log(data)
-                axios.post("/admin/common/uploadImage",data).then(data =>{
-                    console.log(data)
-                    _this.img = './resource/'+data.data.path
-                }).catch((error)=>{
-                    throw error
+            //formData对象
+            let canvasFormData = new FormData()
+            canvasFormData.append("file", blob)
+
+            await axios.post("/admin/common/uploadImage",canvasFormData).then(data =>{
+                _this.canvasImage = './resource/'+data.data.path
+                _this.isImage = true
+                // debugger
+                if(_this.isImage){
+                    _this.isMakingImage = false
+                }
+                
+            }).catch((error)=>{
+                _this.$message({
+                    message: error.message,
+                    type: 'error'
                 })
-            });
+                _this.isMakingImage = false
+            })
+            // await (_this.isMakingImage = false)()
+        });
     }
 }
 </script>
 <style lang="scss" scoped>
 
-.makedImage{
-    position: fixed;
-    top:50%;
-    right: 50%;
-    height: 500px;
-    width: 500px;
-    background: #185;
-}
-.img{
-    position: fixed;
-    z-index: 100;
-    top:60%;
-    right: 70%;
-    height: 500px;
-    width: 500px;
-    background: #185;
-}
+    .imageBox{
+        position: fixed;
+        // z-index: -200;
+        top:calc(50% - 250px);
+        left: calc(50% - 400px);
+        height: 600px;
+        width: 800px;
+        .imageBox-contains{
+            height: 600px;
+            width: 800px;
+            position: relative;
+            .imageCutBox{
+                position: absolute;
+                left: 0;
+                top:0;
+                height: 500px;
+                width: 800px;
+                .canvas-img{
+                    height: 500px;
+                    width: 800px;
+                }
+            }
+            .canvasDiv{
+                background: #185;
+            }
+            .imageBox-footer{
+                 position: absolute;
+                left: 0;
+                bottom:0;
+                height: 100px;
+                width: 100%;
+            }
+        }
+
+    }
 </style>
 
 
