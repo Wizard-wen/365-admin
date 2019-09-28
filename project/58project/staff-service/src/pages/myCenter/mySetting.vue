f98a2194ad17240404343844ef4062541ba82582
wizard <sxw_1994@163.com>
2019-09-28 13:30:40 +0800

    提交

--- project/58project/staff-service/src/pages/myCenter/mySetting.vue
+++ project/58project/staff-service/src/pages/myCenter/mySetting.vue
@@ -1,31 +1,40 @@
 <template>
-<div>
+<div class="mySetting" v-loading="is_loading" >
 
   <el-tabs v-model="currentTabPosition" :tab-position="tabPosition" @tab-click="changeTab">
     <el-tab-pane label="员工基本信息" name="basic">
-        <el-form label-width="120px" ref="baseForm" :model="baseForm" class="baseForm">
+        <el-form label-width="120px" ref="baseForm" :model="baseForm" class="publicForm baseForm">
             <el-form-item label="账号"  prop="account">
-                <el-input v-model="baseForm.account" :maxlength="20" :disabled="true" ></el-input>
+                <!-- <el-input v-model="baseForm.account" :maxlength="20" :disabled="true" ></el-input> -->
+                {{baseForm.account}}
             </el-form-item>
 
             <el-form-item label="工号" prop="manager_code">
-                <el-input autocomplete="off" v-model="baseForm.manager_code"  :disabled="true" :maxlength="20"></el-input>
+                <!-- <el-input autocomplete="off" v-model="baseForm.manager_code"  :disabled="true" :maxlength="20"></el-input> -->
+                {{baseForm.manager_code}}
             </el-form-item>
 
             <el-form-item label="真实姓名" prop="real_name">
-                <el-input autocomplete="off" :disabled="true" v-model="baseForm.real_name" ></el-input>
+                <!-- <el-input autocomplete="off" :disabled="true" v-model="baseForm.real_name" ></el-input> -->
+                {{baseForm.real_name}}
             </el-form-item>
 
             <el-form-item label="所属部门" prop="department_id">
-                <el-input v-model="baseForm.department_id" :disabled="true"></el-input>
+                <!-- <el-input v-model="department_name" :disabled="true"></el-input> -->
+                <select-tag-component
+                    v-if="baseForm.department_id"
+                    :propTagList="departmentList"
+                    v-model="baseForm.department_id"
+                    :isSingle="true"
+                    :isEdit="false"></select-tag-component>
             </el-form-item>
         </el-form>
     </el-tab-pane>
     <el-tab-pane label="员工个人信息" name="personal">
-        <el-form label-width="120px" ref="personalInfoForm" :rules="personalInfoRules" :model="personalInfoForm" class="personalInfoForm">
+        <el-form label-width="120px" ref="personalInfoForm" :rules="personalInfoRules" :model="personalInfoForm" class="publicForm personalInfoForm">
 
-            <el-form-item label="用户名" prop="user_name">
-                <el-input autocomplete="off" v-model="personalInfoForm.user_name" ></el-input>
+            <el-form-item label="用户名" prop="name">
+                <el-input autocomplete="off" v-model="personalInfoForm.name" ></el-input>
             </el-form-item>
             <el-form-item label="头像" class="form-item-size">
                 <el-upload
@@ -60,6 +69,7 @@
             <el-form-item label="生日" prop="birthday">
                 <el-date-picker 
                     :picker-options="datePickerOption" 
+                    :default-value="timeDefaultShow"
                     v-model="personalInfoForm.birthday" 
                     value-format="timestamp" 
                     type="date" 
@@ -82,8 +92,8 @@
                 <el-input v-model="personalInfoForm.urgent"></el-input>
             </el-form-item>
 
-            <el-form-item label="现住址" prop="current_adderss">
-                <el-input v-model="personalInfoForm.current_adderss"></el-input>
+            <el-form-item label="现住址" prop="current_address">
+                <el-input v-model="personalInfoForm.current_address"></el-input>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" @click="changePersonalInfo('personalInfoForm')">确认更改</el-button>
@@ -91,9 +101,9 @@
         </el-form>
     </el-tab-pane>
     <el-tab-pane label="更改密码">
-        <el-form label-width="120px" ref="passwordChangeForm" :rules="passwordChangeRules" :model="passwordChangeForm" class="passwordChangeForm">
-            <el-form-item label="原密码" prop="originPassword">
-                <el-input type="password" v-model="passwordChangeForm.originPassword"></el-input>
+        <el-form label-width="120px" ref="passwordChangeForm" :rules="passwordChangeRules" :model="passwordChangeForm" class="publicForm passwordChangeForm">
+            <el-form-item label="原密码" prop="password">
+                <el-input type="password" v-model="passwordChangeForm.password"></el-input>
             </el-form-item>
             <el-form-item label="新密码" prop="newPassword">
                 <el-input type="password" v-model="passwordChangeForm.newPassword"></el-input>
@@ -113,10 +123,12 @@
 </template>
 <script>
 import {myCenterService} from '../../../common/index.js'
-  export default {
+import {selectTagComponent} from '@/pages/components/index.js'
+   export default {
     data() {
         let _this = this;
         const validate = {
+            //新密码
             newPassword(rule, value, callback){
                 if (value === '') {
                     callback(new Error('请输入密码'));
@@ -127,6 +139,7 @@ import {myCenterService} from '../../../common/index.js'
                     callback();
                 }
             },
+            //重复密码
             reNewPassword(rule, value, callback) {
                 if (value === '') {
                     callback(new Error('请再次输入密码'));
@@ -138,6 +151,7 @@ import {myCenterService} from '../../../common/index.js'
             }
         }
         return {
+            is_loading: false,
             //tab位置
             tabPosition: 'left',
             //tab当前所在页
@@ -151,7 +165,7 @@ import {myCenterService} from '../../../common/index.js'
              * 更改密码字段
              */
             passwordChangeForm: {
-                originPassword: '',//原始密码
+                password: '',//原始密码
                 newPassword: '',//新密码
                 reNewPassword: '',//确认新密码
             },
@@ -159,7 +173,7 @@ import {myCenterService} from '../../../common/index.js'
              * 更改密码表单验证
              */
             passwordChangeRules: {
-                originPassword: [
+                password: [
                     {required: true, message: '请输入原始密码', trigger: 'blur'}
                 ],
                 newPassword: [
@@ -173,26 +187,28 @@ import {myCenterService} from '../../../common/index.js'
             },
             //账号基本信息
             baseForm: {
+                get_for: 'basic',
                 account: '',//账号
                 manager_code: '',//员工号
                 real_name: '',//真实姓名
-                departmeng_id: '',//所属部门
+                department_id: '',//所属部门
             },
             //用户个人信息
             personalInfoForm: {
-                user_name: '',//用户名
+                get_for: 'personal',
+                name: '',//用户名
                 phone: '',//手机号
                 birthday: '',//生日
                 email: '',//邮箱
                 wechat: '',//微信
                 personal_intro: '',//个人简介
                 urgent: '',//紧急联系人
-                current_adderss: '',//现住址
+                current_address: '',//现住址
                 icon: ''
             },
             //个人信息表单验证
             personalInfoRules: {
-                user_name: [
+                name: [
                     {required: true, message: '请输入用户名', trigger: 'blur'}
                 ],
                 phone: [
@@ -204,15 +220,22 @@ import {myCenterService} from '../../../common/index.js'
             },
             icon_fileList: [],//头像数组
             isShowBlack: false,//头像阴影
+            timeDefaultShow:'',//时间选择器默认时间
             //图片上传header
             uploadHeader:{
                 accessToken: this.$store.state.loginModule.token.access_token
             },
         };
     },
+    components: {
+        selectTagComponent,
+    },
     computed: {
         presentUser(){
             return this.$store.state.loginModule.user
+        },
+        departmentList(){
+            return this.$store.state.authModule.departmentList
         }
     },
     methods: {
@@ -227,13 +250,21 @@ import {myCenterService} from '../../../common/index.js'
                         if(param.name == 'basic'){
                             this.baseForm = data.data
                         } else {
-                            this.personalInfoForm = data.data
+                            this.timeDefaultShow = Date.parse(new Date());
+                            this.personalInfoForm = {
+                                ...this.personalInfoForm,
+                                ...data.data
+                            }
+                            // this.personalInfoForm.icon = this.personalInfoForm.icon ? 
+                            //     `./resource/${this.personalInfoForm.icon}` : ''
                         }
                     }).catch(error =>{
                         this.$message({
                             type: 'error',
                             message: error.message
                         })
+                        this.is_loading = false
+                    }).finally(() =>{
                         this.is_loading = false
                     })
                 } catch(error){
@@ -288,16 +319,24 @@ import {myCenterService} from '../../../common/index.js'
          * 更改密码
          */
         async changePassword(formName){
+            let _this = this;
             await this.$refs[formName].validate(async valid =>{
                 if(valid){
                     try{
                         this.is_loading = true
+                        this.passwordChangeForm = {
+                            ...this.passwordChangeForm,
+                            id: _this.presentUser.id
+                        }
                         await myCenterService.changePassword(this.passwordChangeForm).then(data =>{
                             if(data.code == '0'){
                                 this.$message({
                                     type: 'success',
                                     message: data.message
                                 })
+                                this.passwordChangeForm.password = ''
+                                this.passwordChangeForm.newPassword = ''
+                                this.passwordChangeForm.reNewPassword = ''
                                 this.is_loading = false
                             }
                         }).catch(error =>{
@@ -326,7 +365,7 @@ import {myCenterService} from '../../../common/index.js'
          * 重置更改密码表单
          */
         resetNewPasswordForm(){
-            this.passwordChangeForm.originPassword = ''
+            this.passwordChangeForm.password = ''
             this.passwordChangeForm.newPassword = ''
             this.passwordChangeForm.reNewPassword = ''
         },
@@ -342,10 +381,10 @@ import {myCenterService} from '../../../common/index.js'
         },
         //头像上传成功
         iconUploadSuccess(res, file) {
-            this.workerForm.icon = res.data.path;
+            this.personalInfoForm.icon = res.data.path;
 
             this.icon_fileList =  [{
-                url: `./resource/${this.workerForm.icon}`,
+                url: `./resource/${this.personalInfoForm.icon}`,
                 name: 'head'
             }]
         },
@@ -363,6 +402,7 @@ import {myCenterService} from '../../../common/index.js'
             this.is_loading = true
             await myCenterService.getPersonal(this.presentUser.id, 'basic').then(data =>{
                 this.baseForm = data.data
+                this.is_loading = false
             }).catch(error =>{
                 this.$message({
                     type: 'error',
@@ -383,6 +423,13 @@ import {myCenterService} from '../../../common/index.js'
 
 
 <style lang="scss" scoped>
+.mySetting{
+    padding: 24px;
+    .publicForm{
+        width:760px;
+    }
+}
+//图片
 .icon-uploader{
     & /deep/ .el-upload {
         height: 65px;
